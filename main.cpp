41bd8bf8a68efaf2c7cc2184fa3836a422652462
Sam331 <36762547+zyhusesit@users.noreply.github.com>
2022-03-29 10:28:33 +0800

    A better version!

--- main.cpp
+++ main.cpp
@@ -3,27 +3,42 @@
 
 using namespace std;
 
+
+struct NumNode {
+    bool is_last_neighbor = false;
+    bool is_next_neighbor = false;
+};
+
 int main() {
     int N;
-    int sum=0;
     cin >> N;
-    int SourceData[N+1];
-    int TargetData[N+1];
+    int sum = 0;
+
+    int SourceData[N];
+    NumNode TargetData[N + 1];
+
 
     for (int i = 0; i < N; ++i) {
-        SourceData[i] = std::experimental::randint(0, N);
+        SourceData[i] = experimental::randint(0, N);
+        cout << " " << SourceData[i];
     }
+    cout << "\n";
 
-    for (int i = 0; i < N; ++i) {
-        TargetData[SourceData[i]] = SourceData[i];
-    }
-
-    for (int i = 0; i < N; ++i) {
-        if (TargetData[i]&&TargetData[i+1]){
-            cout<<"("<<i<<","<<i+1<<")"<<endl;
+    for (int i = 0; i < N - 1; ++i) {
+        if (SourceData[i] - SourceData[i + 1] == -1) {
+            TargetData[SourceData[i]].is_next_neighbor = true;
+            sum++;
+        } else if (SourceData[i] - SourceData[i + 1] == 1) {
+            TargetData[SourceData[i]].is_last_neighbor = true;
             sum++;
         }
     }
-    cout<<"\n";
-    cout<<"Sum is "<<sum;
+
+    for (int i = 0; i < N + 1; ++i) {
+        if (TargetData[i].is_next_neighbor)
+            cout << "(" << i << "," << i + 1 << ") " << endl;
+        else if (TargetData[i].is_last_neighbor)
+            cout << "(" << i << "," << i - 1 << ") " << endl;
+    }
+    cout << "The sum is: " << sum;
 }
